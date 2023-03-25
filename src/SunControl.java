75ff580539e3e5ca54bdbbc12a80982b9ee38362
jhilla01 <114031625+jhilla01@users.noreply.github.com>
2023-03-24 23:40:10 -0500

    basic sun-movement gui with button functionality.

---
+++ src/SunControl.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SunControl {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new SunFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setTitle("Sun Control");
+            frame.setVisible(true);
+        });
+    }
+}
+
+
+class SunFrame extends JFrame {
+    private SunPanel sunPanel;
+    private JButton upButton, downButton, leftButton, rightButton, dayButton, nightButton;
+
+    public SunFrame() {
+        sunPanel = new SunPanel();
+        JPanel buttonPanel = new JPanel(new GridLayout(2,3));
+
+        upButton = new JButton("Up");
+        upButton.addActionListener(e -> sunPanel.moveSun(0, -10));
+        buttonPanel.add(upButton);
+
+        downButton = new JButton("Down");
+        downButton.addActionListener(e -> sunPanel.moveSun(0, 10));
+        buttonPanel.add(downButton);
+
+        leftButton = new JButton("Left");
+        leftButton.addActionListener(e -> sunPanel.moveSun(-10, 0));
+        buttonPanel.add(leftButton);
+
+        rightButton = new JButton("Right");
+        rightButton.addActionListener(e -> sunPanel.moveSun(10, 0));
+        buttonPanel.add(rightButton);
+
+        dayButton = new JButton("Day");
+        dayButton.addActionListener(e -> sunPanel.setDay(true));
+
+        buttonPanel.add(dayButton);
+
+        nightButton = new JButton("Night");
+        nightButton.addActionListener(e -> sunPanel.setDay(false));
+        buttonPanel.add(nightButton);
+
+        add(sunPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+        pack();
+    }
+}
+
+class SunPanel extends JPanel {
+    private int sunX = 175;
+    private int sunY = 125;
+    private boolean isDay = true;
+    public SunPanel() {
+        setPreferredSize(new Dimension(400, 300));
+    }
+    public void moveSun(int dx, int dy) {
+        sunX += dx;
+        sunY += dy;
+        repaint();
+    }
+
+    public void setDay(boolean isDay) {
+        this.isDay = isDay;
+        repaint();
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (isDay) {
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillRect(0, 0, getWidth(), getHeight());
+            g.setColor(Color.YELLOW);
+        } else {
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(0, 0, getWidth(), getHeight());
+            g.setColor(Color.LIGHT_GRAY);
+        }
+        g.fillOval(sunX, sunY, 50, 50);
+    }
+}
